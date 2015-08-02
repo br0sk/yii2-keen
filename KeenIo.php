a0bc1d0cd3294a8dc0d495f2ee45432e516ea04a
John  Eskilsson <john.eskilsson@outlook.com>
2015-08-02 19:53:29 +0000

    Refactored and added features

--- KeenIo.php
+++ KeenIo.php
@@ -4,47 +4,118 @@ namespace br0sk\keen;
 use yii\base\Component;
 use KeenIO\Client\KeenIOClient;
 
+/**
+ * This comopnent let's you use keen.io in yii2.
+ * 
+ * note:
+ */ 
 class KeenIo extends Component
 {
-	public $projectId = '';
-	public $writeKey = '';
-	public $readKey = '';
-	public $masterKey = null;
-	public $version = null;
     private $_keenio = null;
+    private $_projectId = null;
+    private $_readKey = null;
+    private $_writeKey = null;
+    private $_masterKey = null;
+    private $_version = null;
 
+    public function init()
+    {
+        parent::init();
+        if (!$this->_projectId) {
+            throw new InvalidConfigException('ProjectId cannot be empty!');
+        }
+        if (!$this->_writeKey) {
+            throw new InvalidConfigException('WriteKey cannot be empty!');
+        }
+        if (!$this->_readKey) {
+            throw new InvalidConfigException('Readkey cannot be empty!');
+        }
+        //Create a new Keen object if it hasn't already been created
+        if ($this->_keenio === null) {
+            $configArray = [
+                'projectId' => $this->_projectId,
+                'masterKey' => $this->_masterKey,
+                'writeKey'  => $this->_writeKey,
+                'readKey'   => $this->_readKey,
+            ];
+            //Only add the version if it is properly set, if not use the version the 
+            //PHP SDK choses
+            if($this->_version !== null) {
+                $configArray['version'] = $this->_version;
+            }
+            $this->_keenio = KeenIOClient::factory($configArray);
+        }
+    }
 
-	public function init()
-	{
-		if (!$this->projectId) {
-			throw new InvalidConfigException('ProjectId cannot be empty!');
-		}
-		if (!$this->writeKey) {
-			throw new InvalidConfigException('WriteKey cannot be empty!');
-		}
-		if (!$this->readKey) {
-			throw new InvalidConfigException('Readkey cannot be empty!');
-		}
-	}
+    
+    public function setReadKey($value) {
+        if($this->_keenio !== null) {
+            $this->_keenio->setReadKey($value);
+        }
+        $this->_readKey = $value;
+    }
+    
+    public function getReadKey() {
+        return $this->_readKey;
+    }
+    
+    public function setWriteKey($value) {
+        if($this->_keenio !== null) {
+            $this->_keenio->setWriteKey($value);
+        }
+        $this->_writeKey = $value;
+    }
 
-	public function getKeenio()
-	{
-		if ($this->_keenio === null) {
-			$this->_keenio = KeenIOClient::factory([
-				'projectId' => $this->projectId,
-				'writeKey'  => $this->writeKey,
-				'readKey'   => $this->readKey,
-			]);
-		}
-		return $this->_keenio;
-	}
-	
-	public function __call($method, $params)
-	{
-		$client = $this->getKeenio();
-		if (method_exists($client, $method))
-			return call_user_func_array(array($client, $method), $params);
-		return parent::__call($method, $params);
-	}
+    public function getWriteKey() {
+        return $this->_writeKey;
+    }
+    
+    public function setMasterKey($value) {
+        if($this->_keenio !== null) {
+            $this->_keenio->setMasterKey($value);
+        }
+        $this->_masterKey = $value;
+    }
+
+    public function getMasterKey() {
+        return $this->_masterKey;
+    }
+
+    public function setVersion($value) {
+        if($this->_keenio !== null) {
+            $this->_keenio->setVersion($value);
+        }
+        $this->_version = $value;
+    }
+    public function getVersion() {
+        return $this->_version;
+    }
+    
+    public function setProjectId($value) {
+        if($this->_keenio !== null) {
+            $this->_keenio->setProjectId($value);
+        }
+        $this->_projectId = $value;
+    }
+    public function getProjectId() {
+        return $this->_projectId;
+    }
+    
+    /**
+     * Proxy the calls to the Keen object if the methods doesn't explixitly exist in this class.
+     * See the getters and setters above for methods that shall work directly on this component
+     * rather than on the Keen Object. 
+     * 
+     * If the method called is not found in the Keen Object continue with standard Yii behaviour
+     */ 
+    public function __call($method, $params)
+    {
+        //Override the normal Yii functionality checking the Keen SDK for a matching method
+        if (method_exists($this->_keenio, $method)) {
+            return call_user_func_array(array($this->_keenio, $method), $params);
+        }
+        
+        //Use standard Yii functionality, checking behaviours
+        return parent::__call($method, $params);
+    }
 }
-
--- README.md
+++ README.md
@@ -3,25 +3,23 @@ A Yii2 extension for http://keen.io
 
 This is an extension for Yii2 that makes it easy to use http://keen.io/.
 
-
-
 You can configure it in your application configuration like so:
 
-	'keen' => [
-		'class' => 'br0sk\keen\KeenIo',
-		'projectId' => 'yourprojectid',
-		'readKey' 	=> 'yourreadkey',
-		'writeKey'	=> 'yourwritekey'
-	],
-	
+    'keen' => [
+        'class' => 'br0sk\keen\KeenIo',
+        'projectId' => 'yourprojectid',
+        'readKey' 	=> 'yourreadkey',
+        'writeKey'	=> 'yourwritekey'
+    ],
+    
 **note:** You can find the project id and push API key in the control panel for you project if you log in [here](https://keen.io).
 
 Adding it to your `components` array.
 
 Pushing an event is as easy as:
 
-	$event = ['purchase' => ['item' => 'Golden Elephant']];
-	$keenReturn = Yii::$app->keen->addEvent('purchases', $event);
+    $event = ['purchase' => ['item' => 'Golden Elephant']];
+    $keenReturn = Yii::$app->keen->addEvent('purchases', $event);
     
 You can now use all the calls in the [Keen PHP SDK](https://github.com/keenlabs/KeenClient-PHP) this extension builds on 
 
